98aac7fdba369756d74afa3e578193dbf6736543
natasja de waard <n_kjaersgaard@hotmail.com>
2021-01-11 20:25:52 +0100

    Add project files.

---
+++ myMLApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myMLApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
